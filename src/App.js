89eedbdcd3c8fd3dd83d1084cb596c594f5a4f21
kazeem <kazeem0825@gmail.com>
2020-11-05 08:21:28 +0100

    changed the text in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the street
+          Changes made from the feature branch!
         </p>
         <a
           className="App-link"
